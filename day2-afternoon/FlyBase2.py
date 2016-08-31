bfc14c517c502469734908993251dc79cd079643
Jocelyn Haversat <Jhaversat@gmail.com>
2016-08-30 20:09:23 -0400

    Day 2 Homework problem #2

--- day2-afternoon/FlyBase2.py
+++ day2-afternoon/FlyBase2.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-"""when using make sure to take into account that column number 1 will have trailing spaces"""
+"""Prints the reference gene names from a t_data.ctab file."""
 
 import sys
 
-for line in sys.stdin:
-    if "DROME" in line:
-        stringline = line.rstrip("\n")
-        print stringline[(len(stringline)-23):(len(stringline)-13)], "\t", stringline[(len(stringline)-11):]
-        
-        
-        
-       
+gene_names_dict = {}
+
+f = open(sys.argv[1])
+for line in f:
+    value = line.rstrip( "\r\n").split("\t")
+    gene_names_dict[value[1]] = value[0]
     
-   
-        
+#print gene_names_dict
+    
+g = open(sys.argv[2])    
+for line in g:
+    variable = line.rstrip( "\r\n").split("\t")
+    if variable[8] in gene_names_dict:
+        print variable, gene_names_dict[ variable[8] ]
+    else:
+        if len(sys.argv) > 3:
+            print variable, "x"
+        else:
+            continue   
+    
