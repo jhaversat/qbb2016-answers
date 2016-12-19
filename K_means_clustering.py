52e811f0f01963a9695d7c564a69f850bf9fca33
Jocelyn Haversat <Jhaversat@gmail.com>
2016-12-19 16:54:27 -0500

    Week 11 Homework pt. 3 (Revised with updated plot)
    Plot
    code

--- K_means_clustering.py
+++ K_means_clustering.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import pylab
 from pylab import plot, show
 from numpy import vstack, array
 from numpy.random import rand
@@ -25,6 +26,7 @@ plot(data[idx==0,0],data[idx==0,1],'ob',
      data[idx==1,0],data[idx==1,1],'or')
 plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
 
+pylab.savefig('Kmeans.png')
 
 
 
